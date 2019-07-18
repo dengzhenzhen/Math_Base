c8fa3356064b65f2c536dabdf4e95af7577a8b65
dengzhenzhen <zhenzhendeng2012@gmail.com>
2019-07-18 20:12:58 +0800

    Updated project configs 	modified:   Math_Base/settings.py 	Question_Repository/static/ 	Question_Repository/templates/

--- Math_Base/settings.py
+++ Math_Base/settings.py
@@ -11,10 +11,16 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import Question_Repository.static.static_info as static_info
+import Question_Repository.templates.templates_info as templates_info
+
+STATIC_PATH = static_info.STATIC_PATH
+TEMPLATES_PATH = templates_info.TEMPLATES_PATH
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# print(BASE_DIR)
+# print(static_info.STATIC_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -55,7 +61,7 @@ ROOT_URLCONF = 'Math_Base.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_PATH],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC = [{'DIRS':STATIC_PATH}]
